ff77173db19c9faf2fc60fa35ca4a40d9878f492
Caio Duarte's UX305U <caiosduarte@gmail.com>
2020-07-31 18:40:02 -0300

    organização do codigo com a retirada de classer inúteis

--- src/MessageHandler/RemoveUserMessageHandler.php
+++ src/MessageHandler/RemoveUserMessageHandler.php
@@ -7,8 +7,6 @@ use App\Message\RemoveUserMessage;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 use Doctrine\ORM\EntityManagerInterface;
 
-use App\Entity\User;
-
 final class RemoveUserMessageHandler implements MessageHandlerInterface
 {
     protected EntityManagerInterface $manager;
@@ -20,9 +18,6 @@ final class RemoveUserMessageHandler implements MessageHandlerInterface
 
     public function __invoke(RemoveUserMessage $message)
     {
-        //$getUserService = new GetUserService($this->manager);
-        //$user = $getUserService->execute($userId); 
-
         $user = $this->bus->dispatch(new GetUserMessage($message->getId()));
         
         $this->manager->remove($user);
